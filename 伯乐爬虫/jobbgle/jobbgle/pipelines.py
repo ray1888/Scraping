f332ae45239eb91b27232a5fb2ddcbad6f9cc3e3
ray1888 <[alal1995@hotmail.com]>
2017-06-30 17:59:43 +0800

    first commit

--- 伯乐爬虫/jobbgle/jobbgle/pipelines.py
+++ 伯乐爬虫/jobbgle/jobbgle/pipelines.py
@@ -5,7 +5,19 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
 
 class JobbglePipeline(object):
+    def __init__(self):
+        client = pymongo.MongoClient(host = "localhost", port = 27017)
+        db = client['Jobb']
+        self.job = db['job']
+
+
     def process_item(self, item, spider):
+        try:
+            self.job.insert(dict(item))
+        except Exception:
+            pass
+
         return item
